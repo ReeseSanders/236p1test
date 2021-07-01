4ed67d124d67b9de804f0a1e5486b519ca63d2d0
ReeseSanders <84603494+ReeseSanders@users.noreply.github.com>
2021-06-30 21:27:09 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
 
     std::string readFile = argv[1];
     std::ifstream infile;
-    infile.open(Lab1PassOffCases\readFile");
+    infile.open(Lab1PassOffCases/readFile);
     std::string input((std::istreambuf_iterator<char>(infile)),
                         (std::istreambuf_iterator<char>()));
 
